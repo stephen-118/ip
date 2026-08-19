a634c634a5fef0a9a9664191f04a91a576c0ad0b
Stephen Zhu <e1513126@u.nus.edu>
2026-08-19 22:58:43 +0800

    Add greeting

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -1,6 +1,10 @@
 public class Stephen {
     public static void main(String[] args) {
-        String banner = "Hello! I'm Stephen.";
-        System.out.println(banner);
+        String line1 = "Hello! I'm Stephen.";
+        String line2 = "What can I do for you?";
+        String line3 = "\nBye. Hope to see you again soon!";
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
     }
 }
