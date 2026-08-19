bab4b495e3553a09b454332d3adbc97a32558ee0
Stephen Zhu <e1513126@u.nus.edu>
2026-08-19 17:16:26 +0800

    Rename: Duke -> Stephen

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -1,10 +1,6 @@
-public class Duke {
+public class Stephen {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = "Hello! I'm Stephen.";
         System.out.println(banner);
     }
 }
