0a58836f05b7003c7fbba27106c418fab931a817
Stephen Zhu <e1513126@u.nus.edu>
2026-08-20 11:31:08 +0800

    Add unmark command for task reopening
    Allow users to reverse a task's completion state and confirm the updated not-done status.

--- src/main/java/Stephen.java
+++ src/main/java/Stephen.java
@@ -45,6 +45,11 @@ public class Stephen {
                 isDone[taskIndex] = true;
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("[X] " + tasks[taskIndex]);
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                isDone[taskIndex] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + tasks[taskIndex]);
             } else {
                 tasks[taskCount] = input;
                 taskCount++;
